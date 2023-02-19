a1358ed422bfc80884b396d030f55de359685168
Arnau Quera-Bofarull <arnauq@protonmail.com>
2023-02-19 15:15:38 +0000

    set epsilon to same value as PyTorch

--- src/gumbel_softmax.jl
+++ src/gumbel_softmax.jl
@@ -13,9 +13,9 @@ function sample_gumbel(probs, size; epsilon=1e-10)
     return ret
 end
 
-function sample_gumbel_softmax(probs, tau; hard=true)
-    logits = log.(probs .+ 1e-16)
-    y = logits + sample_gumbel(probs, size(logits))
+function sample_gumbel_softmax(probs, tau; hard=true, epsilon=1e-10)
+    logits = log.(probs .+ epsilon)
+    y = logits + sample_gumbel(probs, size(logits), epsilon=epsilon)
     y_soft = softmax(y / tau, dims=2)
     if hard
         y_hard = (y_soft .== maximum(y_soft, dims=2))
