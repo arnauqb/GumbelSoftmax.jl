9b61a932958bdde526aa5a341f5c2cc4a3f4c378
Arnau Quera-Bofarull <arnauq@protonmail.com>
2023-08-02 21:40:28 +0100

    fixed partial size

--- src/gumbel_softmax.jl
+++ src/gumbel_softmax.jl
@@ -15,7 +15,7 @@ end
 
 stop_gradient(x) = ChainRulesCore.ignore_derivatives(x)
 function stop_gradient(n::ForwardDiff.Dual{T}) where {T}
-    ForwardDiff.Dual{T}(ForwardDiff.value(n), 0.0)
+    ForwardDiff.Dual{T}(ForwardDiff.value(n), ForwardDiff.partials(1))
 end
 stop_gradient(n::Array{<:ForwardDiff.Dual}) = stop_gradient.(n)
 
